f1734fe3a7cf16bda7d6df7ee6afcf474db92739
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2017-07-20 23:10:57 +0200

    Adding info about library in navbar

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,17 +4,6 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.catalog, name='catalog_url'),
-    # url(r'^books/$', views.BookListView.as_view(), name='all_books_url'),
-    # url(r'^book/(?P<pk>\d+)$', views.book_detail_view, name='book_detail_url'),
-    # url(r'^book/$', views.isbn_page, name='book_isbn_url'),
-    # url(r'^mybooks/$', views.UsersBookListView.as_view(), name='users_books_url'),
-    # url(r'^mybooks/delete/(?P<pk>\d+)$', views.BookInstanceDelete.as_view(), name='users_books_delete_url'),
-    # url(r'^mybooks/edit/(?P<pk>\d+)$', views.BookInstanceUpdate.as_view(), name='users_books_update_url'),
-    # # url(r'^mybooks/create/$', views.BookInstanceCreate.as_view(), name='users_book_create_url'),
-    # url(r'^mybooks/create/$', views.create_book_instance, name='users_book_create_url'),
-    # url(r'^mybooks/createFromApi/$', views.create_book_from_api, name='users_book_create_from_api_url'),
-
     url(r'^$', views.catalog, name='catalog_url'),
     url(r'^books/$', views.BookListView.as_view(), name='all_books_url'),
     url(r'^book/(?P<pk>\d+)$', views.book_detail_view, name='book_detail_url'),
